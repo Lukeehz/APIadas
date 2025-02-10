8c475b0f1932b57614fe799f9f6ddad6b65050ff
Lucas Gabriel <lucas.gab1010@gmail.com>
2025-02-10 17:05:18 -0300

    original index

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import express from "express";
-import path from "path";
+import path from "path"; 
 import cors from "cors";
-import router from "./routers/rotas.mjs";  // removido .mts
-import apiadas from "./routers/apidas.mjs"; // removido .mts
-import err404 from "./routers/err404.mjs"; // removido .mts
-import logger from "./log/logger";
+import router from "./routers/rotas.mts";
+import apiadas from "./routers/apidas.mts";
+import err404 from "./routers/err404.mts";
+import logger from "./log/logger.ts"
 import { fileURLToPath } from 'url';
 
+
 const app = express();
-const port = process.env.PORT || 80; // permitir porta dinâmica do Vercel
+const port = 80;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const basePath = path.join(__dirname, "../public/");
-
 app.use(express.static(path.join(__dirname, '../public')));
+
 console.log(basePath);
+
 app.use(express.json());
 app.use(cors());
+
 app.use("/", router);
 app.use("/apiadas", apiadas);
 app.use(err404);
@@ -28,10 +32,5 @@ const dbPassword = process.env.DB_PASS;
 
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@apiadas.3o9p7.mongodb.net/?retryWrites=true&w=majority&appName=Apiadas`)
 .then(() => {
-    if (process.env.NODE_ENV !== 'production') {
-        app.listen(port, '0.0.0.0', () => console.log(`Servidor rodando na porta ${port}`));
-    }
+    app.listen(port, '0.0.0.0', () => console.log(`Servidor rodando na porta ${port} acesse http://meusite.local:${port}`));
 });
-
-// Exportação necessária para o Vercel
-export default app;
